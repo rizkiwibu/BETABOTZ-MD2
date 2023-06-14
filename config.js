d76bab1f40cbfb85e39b757eb9d5fb6f384027b7
Rizki irfan <98686896+rizkiwibu@users.noreply.github.com>
2023-06-14 10:12:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ global.author = 'Bot WhatsApp'
 global.read = false//Untuk autoread kalau pengen autoread nyala true kalau pengen mati false 
 //==> Apikey
 global.lann = 'Your_Key_Here' //isi apikey mu https://api.betabotz.org
-global.btc = 'Your_Key_Here'//Isi apikey di https://api.botcahx.live
+global.btc = 'klegr86M'//Isi apikey di https://api.botcahx.live
 global.APIs = {   
   lann: 'https://api.betabotz.org',
   btc: 'https://api.botcahx.live'
